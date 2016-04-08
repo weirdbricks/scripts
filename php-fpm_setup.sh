9dbbf8180971d78d519e4f540447b128c6588ae9
Lampros <weirdbricks@users.noreply.github.com>
2016-04-07 21:15:54 -0700

    Update php-fpm_setup.sh

--- php-fpm_setup.sh
+++ php-fpm_setup.sh
@@ -6,10 +6,10 @@ CHANGED="[   CHANGED   ] -"
 # check if the php-fpm packages are installed
 package=php-fpm
 if [ `rpm -qa | grep -i $package -c` -eq 0 ]; then
-        echo "$CHANGED $package is not installed, installing.."
-        yum install -y -q $package
+	echo "$CHANGED $package is not installed, installing.."
+	yum install -y -q $package
 else
-        echo "$OK $package is installed"
+	echo "$OK $package is installed"
 fi
 
 package=php-gd
@@ -26,4 +26,13 @@ if [ `rpm -qa | grep -i $package -c` -eq 0 ]; then
         yum install -y -q $package
 else
         echo "$OK $package is installed"
+fi
+
+file=/etc/php-fpm.d/www.conf
+url=https://raw.githubusercontent.com/weirdbricks/scripts/master/www.conf
+if [ `grep "listen = 127.0.0.1:9000" /etc/php-fpm.d/www.conf -c` -ne 1 ]; then
+	echo "$CHANGED $file is not correct, getting a new one.."
+	curl -s $url -o $file
+else
+	echo "$OK $file is correct!"
 fi
